e0c3db5046d6e1bfdbc49229041f62c68b1e0164
siddiq3004 <99350443+siddiq3004@users.noreply.github.com>
2022-06-10 17:57:49 +0530

    Create Day_1.cpp

---
+++ Day_1.cpp
@@ -0,0 +1,29 @@
+/* Given a time in -hour AM/PM format, convert it to military (24-hour) time.*/
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int hr;
+    hr=((s[0]-'0')*10)+(s[1]-'0');
+    if(s[8]=='P'&& s[9]=='M'&& hr==12){
+        cout<<to_string(hr);
+    }
+    else if(s[8]=='P'&& s[9]=='M'){
+        cout<<to_string(hr+12);
+    }
+    else if(s[8]=='A'&& s[9]=='M'&& hr==12){
+        cout<<"00";
+    }
+    else{
+        cout<<s[0]<<s[1];
+    }
+    for(int i=2;i<8;i++){
+        cout<<s[i];
+    }
+    cout<<endl;
+    return 0;
+}
