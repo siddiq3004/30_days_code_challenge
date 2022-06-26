b65ce398581862cbd283179322fa9f5d9e13dd04
siddiq3004 <99350443+siddiq3004@users.noreply.github.com>
2022-06-26 19:34:16 +0530

    Create Day_8.cpp

---
+++ Day_8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+//linear probing with hashing
+int main()
+{
+   int i,m,k,h;
+   int size=200,arr[size];
+   cout<<"enter the size of an array :";
+   cin>>m;
+   cout<<"enter the key element in to an array : ";
+   cin>>k;
+   int h1= k % m;
+   h=((h1 + i) % (m));
+   for(i=0;i<m;i++){
+       if()
+       cout<<h<<"at index "<<i;
+   }
+   
+
+    return 0;
+}
+
